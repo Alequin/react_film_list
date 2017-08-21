1af7edc87fe597ccd9c13538f804ffe6ab20f890
James Cox <j-cox03@hotmail.com>
2017-08-22 00:51:08 +0100

    Made and used FilmList component

---
+++ client/src/components/FilmList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class FilmList extends React.Component{
+  render(){
+    return (
+      <div className="film-list">
+        List of films
+      </div>
+    );
+  }
+}
+
+export default FilmList;
--- client/src/containers/FilmBox.jsx
+++ client/src/containers/FilmBox.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FilmTitle from "./../components/FilmTitle";
+import FilmList from "./../components/FilmList";
 
 class FilmBox extends React.Component{
   render(){
     return (
       <div>
         <FilmTitle>UK Opening This Week</FilmTitle>
+        <FilmList/>
       </div>
     );
   }
