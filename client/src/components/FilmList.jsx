3d04822b4c1b4f38f4259eccd4b4213c453d4f54
James Cox <j-cox03@hotmail.com>
2017-08-22 01:03:51 +0100

    Made state in film-box container and passed to film list component

--- client/src/components/FilmList.jsx
+++ client/src/components/FilmList.jsx
@@ -4,8 +4,10 @@ import Film from "./../components/Film";
 
 class FilmList extends React.Component{
   render(){
+    console.log(this.props.data);
     return (
       <div className="film-list">
+        <Film/>
         <Film/>
       </div>
     );
--- client/src/containers/FilmBox.jsx
+++ client/src/containers/FilmBox.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import FilmTitle from "./../components/FilmTitle";
 import FilmList from "./../components/FilmList";
 
+const films = ["Sausage Party", "Cafe Society", "Morgan"];
+
 class FilmBox extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      data: films
+    }
+  }
+
   render(){
     return (
       <div className="film-box">
         <FilmTitle>UK Opening This Week</FilmTitle>
-        <FilmList/>
+        <FilmList data={this.state.data}/>
       </div>
     );
   }
